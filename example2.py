3dad5dd62a6bae8a07deaedacb66fb0e50831799
Chris Yang <cyang@deltalearns.ca>
2022-05-25 09:48:06 -0700

    Update example2.py
    updated with event binding instead of parameters

--- example2.py
+++ example2.py
@@ -12,7 +12,18 @@ win = tk.Tk()
 eoutput = StringVar()
 eoutput.set("Output goes here")
 
-def clickFunction():
+def clickFunction(event):
+    """ 
+    This function is triggered by an EVENT
+    Many events can make use of the properties of an object
+    that it is bound to, so the object and the event make use of 
+    an input parameter. In this case, we are storing the event
+    details in the variable "event"
+
+    """
+    # This command will show you some of the information about the event
+
+    print(event)
     number = e1.get()
     number = float(number)
 
@@ -31,7 +42,14 @@ def clickFunction():
 
 l1 = Label(win, text="Enter a number")
 e1 = Entry(win, width=20)
-b1 = Button(win, text="Click to double", command=clickFunction)
+b1 = Button(win, text="Click to double")
+# Once we create the button widget, we can bind an event to it
+# this means that when the specified action is triggered,
+# the corresponding event handler function is executed
+# see some of the different actions that can trigger events:
+# https://python-course.eu/tkinter/events-and-binds-in-tkinter.php
+b1.bind("<Button>",clickFunction)
+
 a_label = Label(win, text="The entry doubled is:")
 a_entry = Entry(win, width=20, textvariable=eoutput)
 
